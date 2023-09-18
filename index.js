fa3a2b58504c90130ee9977d09da78df976423c9
Gilbert Omosa <gilbert.omosa@student.moringaschool.com>
2023-09-18 18:53:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-
 // Write your code here
+    {
+    const num1 = 31;
+    const num2 = 2;
+
+    const multiply = num1 * num2;
+    }
+
+    {
+    const random = Math.floor(Math.random() * 100) + 1;
+    }
+
+    {
+    const num3 = 29;
+    const num4 = 5;
+
+    const mod = (num3 % num4);
+    }
+
+    {
+    const numbers = [10, 15, 20, 5, 12]; 
+    const max = Math.max(...numbers);
+
+    }
